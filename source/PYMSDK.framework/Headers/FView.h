962d0822fbdd3c0f9bccb081fb9169cbd493c97e
pangyiming <pangyiming@qingting.fm>
2019-06-14 17:27:34 +0800

    feature:修改podspect

---
+++ source/PYMSDK.framework/Headers/FView.h
@@ -0,0 +1,17 @@
+//
+//  FView.h
+//  PYMSDK
+//
+//  Created by 庞伊明 on 2019/6/14.
+//  Copyright © 2019 Markphone Culture Media Co.Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FView : UIView
+- (id)initWithFrame:(CGRect)frame;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ source/PYMSDK.framework/Headers/PYMSDK.h
@@ -0,0 +1,11 @@
+//
+//  PYMSDK.h
+//  PYMSDK
+//
+//  Created by 庞伊明 on 2019/6/14.
+//  Copyright © 2019 Markphone Culture Media Co.Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "FView.h"
+#import "ViewController1.h"
